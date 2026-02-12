31295802515d0eb34e248cbdc0d87e6b99322f98
Dzmitry Sazonau <dzmitry_sazonau1@epam.com>
2026-02-12 10:22:18 +0300

    refactoring GildedRose

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,7 +1,6 @@
 package com.gildedrose;
 
 import java.util.List;
-import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -12,35 +11,35 @@ class GildedRose {
     private static final String CONJURED_ITEMS_REGEX = "Conjured";
     private static final int MAX_QUALITY = 50;
     private static final int MIN_QUALITY = 0;
-    private static final Set<String> LEGENDARY_GOODS_NOT_FOR_SALE = Set.of(SULFURAS_HAND_OF_RAGNAROS.toLowerCase());
 
     private ItemProcessor defaultProcessor = new ItemProcessor(it -> it.sellIn < MAX_QUALITY + 1, it -> {
-        validateAndDecrementQuality(it);
-        decrementSellIn(it);
-        if (it.sellIn < 0) validateAndDecrementQuality(it);
+        adjustQuality(it, -1);
+        it.sellIn--;
+        if (it.sellIn < 0) adjustQuality(it, -1);
     });
 
-    private ItemProcessor legendaryGoodsProcessor = new ItemProcessor(it -> LEGENDARY_GOODS_NOT_FOR_SALE.contains(it.name.toLowerCase()),
-        it -> {});
+    private ItemProcessor legendaryGoodsProcessor = new ItemProcessor(it -> SULFURAS_HAND_OF_RAGNAROS.equalsIgnoreCase(it.name),
+        it -> {
+        });
 
     private ItemProcessor agedBrieProcessor = new ItemProcessor(it -> AGED_BRIE.equalsIgnoreCase(it.name), it -> {
-        validateAndIncrementQuality(it);
-        decrementSellIn(it);
-        if (it.sellIn < 0) validateAndIncrementQuality(it);
+        adjustQuality(it, 1);
+        it.sellIn--;
+        if (it.sellIn < 0) adjustQuality(it, 1);
     });
 
     private ItemProcessor backstagePassesProcessor = new ItemProcessor(it -> it.name.startsWith(BACKSTAGE_PASSES_REGEX), it -> {
-        validateAndIncrementQuality(it);
-        if (it.sellIn < 11) validateAndIncrementQuality(it);
-        if (it.sellIn < 6) validateAndIncrementQuality(it);
-        decrementSellIn(it);
+        adjustQuality(it, 1);
+        if (it.sellIn < 11) adjustQuality(it, 1);
+        if (it.sellIn < 6) adjustQuality(it, 1);
+        it.sellIn--;
         if (it.sellIn < 0) it.quality = MIN_QUALITY;
     });
 
     private ItemProcessor conjuredItemsProcessor = new ItemProcessor(it -> it.name.startsWith(CONJURED_ITEMS_REGEX), it -> {
-        validateAndDecrementQuality(it, 2);
-        decrementSellIn(it);
-        if (it.sellIn < 0) validateAndDecrementQuality(it, 2);
+        adjustQuality(it, -2);
+        it.sellIn--;
+        if (it.sellIn < 0) adjustQuality(it, -2);
     });
     private List<ItemProcessor> customizedItemProcessors = List.of(legendaryGoodsProcessor, agedBrieProcessor, backstagePassesProcessor, conjuredItemsProcessor);
 
@@ -61,26 +60,8 @@ class GildedRose {
         }
     }
 
-    private void validateAndDecrementQuality(Item item) {
-        if (item.quality > MIN_QUALITY) {
-            item.quality = item.quality - 1;
-        }
-    }
-
-    private void validateAndDecrementQuality(Item item, int multiplier) {
-        for (int i = 0; i < multiplier; i++) {
-            validateAndDecrementQuality(item);
-        }
-    }
-
-    private void validateAndIncrementQuality(Item item) {
-        if (item.quality < MAX_QUALITY) {
-            item.quality = item.quality + 1;
-        }
-    }
-
-    private void decrementSellIn(Item item) {
-            item.sellIn = item.sellIn - 1;
+    static void adjustQuality(Item item, int delta) {
+        item.quality = Math.max(MIN_QUALITY, Math.min(MAX_QUALITY, item.quality + delta));
     }
 
     private class ItemProcessor {
